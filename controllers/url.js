6f5f37fe1864359600b199a6cfb151758c305278
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-02-06 23:13:42 +0530

    revert back

--- controllers/url.js
+++ controllers/url.js
@@ -15,34 +15,34 @@ async function handlegenerateurlshortnerurl(req, res) {
     const newcreated = await url.create({
         sortid: newid,
         redirectul: body.url,
-        //     visithistory: []
-        // });
-        // // console.log(newcreated);
-        // // return res.status(200).json({ id: newid });
-        // //If we want that each and every time i got the table:
-        // return res.render('home' ,{
-        //     id:newid,
-        //     urls: allurls
-        // } )
-    }
+        visithistory: []
+    });
+    // console.log(newcreated);
+    // return res.status(200).json({ id: newid });
+    //If we want that each and every time i got the table:
+    return res.render('home', {
+        id: newid,
+        urls: allurls
+    })
+}
 
 async function gettherealurl(req, res) {
-            const furl = req.params.id
-            const changed = await url.findOneAndUpdate({
-                sortid: furl
-            },
-                { $push: { visithistory: { timestamps: new Date() } } },
-                { new: true })
-            // console.log(changed + "and the furl is" + furl);
-            res.status(200).redirect(changed.redirectul)
-        }
+    const furl = req.params.id
+    const changed = await url.findOneAndUpdate({
+        sortid: furl
+    },
+        { $push: { visithistory: { timestamps: new Date() } } },
+        { new: true })
+    // console.log(changed + "and the furl is" + furl);
+    res.status(200).redirect(changed.redirectul)
+}
 
 async function handletheanalysis(req, res) {
-            const jenamatejovuchee = req.params.shortedurl
-            const finded = await url.findOne({
-                sortid: jenamatejovuchee
-            })
-            res.status(200).json({ totalclicksontheurl: `${finded.visithistory.length}` })
-        }
+    const jenamatejovuchee = req.params.shortedurl
+    const finded = await url.findOne({
+        sortid: jenamatejovuchee
+    })
+    res.status(200).json({ totalclicksontheurl: `${finded.visithistory.length}` })
+}
 
 export { handlegenerateurlshortnerurl, gettherealurl, handletheanalysis }; // Export using ES Module syntax
